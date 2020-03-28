9171d3a7595ead2bcd5e94c1c6abd9f27768f081
ashim-bhatta <60122258+ashim-bhatta@users.noreply.github.com>
2020-03-28 15:38:03 +0545

    Add files via upload

--- sk96/js/main.js
+++ sk96/js/main.js
@@ -1,32 +1,72 @@
 const outerCursor = document.getElementById('outerCursor')
 const cursor = document.getElementById('cursor')
+const hamMenu = document.getElementById('ham-menu')
+const nav = document.getElementById('nav')
+const menu = document.getElementById('menu')
 
+
+/*****getting height of outer div */
+outerCursorHeight = () => {
+    height = outerCursor.offsetHeight
+    h = height / 2
+    return (h)
+}
+
+
+/*******Event listener to mouse move ****/
 document.getElementById("body").addEventListener("mousemove", function(event) {
     myFunction(event);
 });
 
 
-function myFunction(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    var coor = "Coordinates: (" + x + "," + y + ")";
-    changeDivPosition(x, y)
+myFunction = (e) => {
+    x = e.clientX;
+    y = e.clientY
+    changeCursorPosition(x, y)
+}
+
+/*****cuctom cursor position */
+
+changeCursorPosition = (x, y) => {
+    h = outerCursorHeight()
+    outerCursor.style.top = (y - h) + 'px'
+    outerCursor.style.left = (x - h) + 'px'
+    cursor.style.top = (y) + 'px'
+    cursor.style.left = (x) + 'px'
+
 
 }
-changeDivPosition = (x, y) => {
-    outerCursor.style.top = (y - 8) + 'px'
-    outerCursor.style.left = (x - 8) + 'px'
-    cursor.style.top = (y + 5) + 'px'
-    cursor.style.left = (x + 5) + 'px'
 
-
-}
-const cursorHover = () => {
+/*****change size of outer div on hover */
+const cursorHover = (x, y) => {
     outerCursor.style.height = '50px'
     outerCursor.style.width = '50px'
+    outerCursor.style.border = '2px solid white'
+
+
 }
 
-const normalCursor = () => {
+const normalCursor = (x, y) => {
     outerCursor.style.height = '30px'
     outerCursor.style.width = '30px'
+    cursor.style.height = '5px'
+    cursor.style.width = '5px'
+    outerCursor.style.border = '1px solid white'
+
+}
+
+/*******toogle mobile menu */
+var isMenuOpen = false;
+hamMenuToogle = () => {
+    if (isMenuOpen === true) {
+        nav.style.height = '100vh'
+        nav.style.backgroundColor = 'rgba(255, 94, 0, 0.486)'
+        isMenuOpen = false
+    } else {
+        nav.style.height = '15vh'
+        isMenuOpen = true
+        nav.style.backgroundColor = '#0F0E0D'
+
+    }
+
 }
