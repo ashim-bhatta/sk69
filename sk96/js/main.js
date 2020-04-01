c3bee50d43643a0028c9007826bbc9a47c2be6cd
ashim-bhatta <60122258+ashim-bhatta@users.noreply.github.com>
2020-04-01 21:14:03 +0545

    Add files via upload

--- sk96/js/main.js
+++ sk96/js/main.js
@@ -3,6 +3,8 @@ const cursor = document.getElementById('cursor')
 const hamMenu = document.getElementById('ham-menu')
 const nav = document.getElementById('nav')
 const mobileMenu = document.getElementById('mobile-menu')
+// const img = document.querySelectorAll('#img')
+const box = document.querySelectorAll('#box');
 
 
 /*****getting height of outer div */
@@ -16,13 +18,14 @@ outerCursorHeight = () => {
 /*******Event listener to mouse move ****/
 document.getElementById("body").addEventListener("mousemove", function(event) {
     myFunction(event);
+
 });
 
 
 myFunction = (e) => {
     x = e.clientX;
     y = e.clientY
-    changeCursorPosition(x, y)
+    changeCursorPosition(x, y);
 }
 
 /*****cuctom cursor position */
@@ -37,21 +40,23 @@ changeCursorPosition = (x, y) => {
 
 }
 
-/*****change size of outer div on hover */
+
+/*****change size of outer div on mouse over */
 const cursorHover = (x, y) => {
-    outerCursor.style.height = '50px'
-    outerCursor.style.width = '50px'
-    outerCursor.style.border = '2px solid white'
-
-
+    outerCursor.style.height = '70px'
+    outerCursor.style.width = '70px'
+    outerCursor.style.border = '2px solid rgb(255, 94, 0)';
+    cursor.style.border = '4px solid rgb(255, 94, 0)';
+    cursor.style.background = 'rgb(255, 94, 0)';
 }
-
+/*****change size of outer div on mouse out  */
 const normalCursor = (x, y) => {
     outerCursor.style.height = '30px'
     outerCursor.style.width = '30px'
+    outerCursor.style.border = '1px solid white';
     cursor.style.height = '5px'
     cursor.style.width = '5px'
-    outerCursor.style.border = '1px solid white'
+    cursor.style.border = '1px solid rgb(255, 94, 0)';
 
 }
 
@@ -67,5 +72,27 @@ hamMenuToogle = () => {
         isMenuOpen = true
 
     }
-
 }
+
+
+changeCursorPosition = (x, y) => {
+    h = outerCursorHeight();
+
+    /****for outer cursor */
+    outerCursor.style.top = y - h + 'px';
+    outerCursor.style.left = x - h + 'px';
+
+    /****for main cursor */
+    cursor.style.top = y + 'px';
+    cursor.style.left = x + 'px';
+    
+
+    /***for image  */
+    var i = 0;
+    for (i = 0; i < box.length; i++) {
+            box[i].style.top = y + 'px';
+            box[i].style.left = x + 'px';
+
+    }
+};
+
